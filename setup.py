ab099bf83abc8a46be17201a8d237d653ba38a3b
Kemey.RU <kemey@163.com>
2018-02-17 23:13:09 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,16 +15,16 @@ extension = Extension(
         "geo.cc",
         "rugis_python.cc"
         ],
-    include_dirs=['/usr/lib','/usr/local/lib','3rdparty', '3rdparty/google','3rdparty/boost_1_58_0/include'],
+    include_dirs=['/usr/lib','/usr/lib64','/usr/local/lib','3rdparty', '3rdparty/google','3rdparty/boost_1_58_0/include'],
     libraries=['s2', 'crypto'],
     library_dirs=['3rdparty/lib'],
-    runtime_library_dirs=['.', './rugis_libs','/usr/lib','/usr/local/lib'],
+    runtime_library_dirs=['.', './rugis_libs','/usr/lib','/usr/lib64','/usr/local/lib'],
     extra_compile_args=['-std=c++11', '-fno-wrapv', '-stdlib=libc++'],
     define_macros=[('NDEBUG', '1'), (os_macro, None)]
     )
 
 setup(
     name="rugis",
-    version='2.0',
+    version='1.0',
     description='preprocess module for project radar',
     ext_modules=[extension])
